cad854f9027d760ca5a1ea210c6bd66d7dca60ba
AudouxH <hugo.audoux@epitech.eu>
2022-04-13 08:55:00 +0000

    add weather api key to get weather

--- soleil-app-web/src/hooks/CallWeatherApi.jsx
+++ soleil-app-web/src/hooks/CallWeatherApi.jsx
@@ -3,7 +3,7 @@ const axios = require('axios');
 export default function callWeatherApi() {
     
     const getWeather = async (cityName) => {
-        const weatherApi = "/* Get your api key from openweathermap.org*/";
+        const weatherApi = "85697833375c27c8e8c4af6f8b935823";
         const pointPosition = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName.split(' ').join('-') + "&appid=" + weatherApi;
         try {
             const position = await axios.get(pointPosition);
